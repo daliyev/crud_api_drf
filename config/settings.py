88fbcf15f631d7c4e88b84969862ad1c6961c97a
daliyev <daliyevazizbekk@gmail.com>
2023-11-28 10:51:13 +0000

    traditional Django

--- config/settings.py
+++ config/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'djangorestframevork',
+    'rest_framework',
     'mydrf1'
 ]
 
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
